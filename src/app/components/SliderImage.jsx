8d8e7c860e4089cf062096fbb534ed0f1f6211a5
Kyaw Lwin Soe <kyawlwinsoe@globalwave.company>
2024-07-02 11:08:20 +0630

    next issue solve

--- src/app/components/SliderImage.jsx
+++ src/app/components/SliderImage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../styles/Slideshow.css';
+import '@/app/styles/Slideshow.css';
 import {useMediaQuery} from "react-responsive";
 import Image from "next/image"; // Import your custom CSS for Slideshow
 
